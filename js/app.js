a891c620875829904ebef04d8961ce7e50ba0225
EstrellaBF <stern.von.nord@gmail.com>
2017-11-28 18:24:25 -0500

    editando js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-/*
- * Funcionalidad de tu producto
- */
+window.addEventListener('load', function(){
+
+});
+
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
+
+
 console.log(data);
+console.log(Object.keys(data)); /*(4) ["AQP", "CDMX", "LIM", "SCL"] */
+console.log(Object.keys(data.AQP));
+console.log(Object.keys(data.CDMX));
+console.log(Object.keys(data.LIM));
+console.log(Object.keys(data.SCL));
+
