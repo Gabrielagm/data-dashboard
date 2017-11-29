73cc45c23543afc546b2dea521e22680b9fc1fbf
EstrellaBF <stern.von.nord@gmail.com>
2017-11-28 23:52:37 -0500

    editando el filtro de generacion

--- js/app.js
+++ js/app.js
@@ -1,15 +1,57 @@
-window.addEventListener('load', function(){
-
+window.addEventListener('load', function(event){
+  var chooseGeneration = document.querySelector('.choose-sede-generation');
+  console.log(chooseGeneration);
+  
+  chooseGeneration.addEventListener('click', function(e){
+    var count;
+	/*Vamos a recorrer las SEDES*/
+	for(var i = 0; i <= Object.keys(data).length; i++){
+	  count++;
+      var listCity = document.createElement('option');
+	  var listText = document.createTextNode('bla');
+	  listCity.setAttribute("value","bla");
+	  listCity.appendChild(listText);
+	  chooseGeneration.appendChild(listCity);
+	}
+  });
+  
 });
 
+/*
+var nuevo = data.LIM["2017-2"].students;
+console.log(nuevo);// 14 alumnos (Cada alumno 4 objetos)
+
+console.log(nuevo[0].active);
+
+var alumnos = Object.keys(nuevo);//["name", "photo", "active", "sprints"]
+
+
+
+
+
+
+var cont=0;
+debugger
+for (var i = 0; i<=nuevo.length; i++) {
+   if (nuevo[i]['active']=== true){
+       cont++;
+   }
+}
+console.log(cont);
+
+/*
+
+var cont=0;
+for(var i = 0; i<=nuevo.length; i++){
+   for(var j = 0 ; j<= alumnos[j].length; j++){
+       if(Object.keys(nuevo[i]).active){
+           cont++;
+       }
+   }
+}
+*/
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 
 
 console.log(data);
-console.log(Object.keys(data)); /*(4) ["AQP", "CDMX", "LIM", "SCL"] */
-console.log(Object.keys(data.AQP));
-console.log(Object.keys(data.CDMX));
-console.log(Object.keys(data.LIM));
-console.log(Object.keys(data.SCL));
-
