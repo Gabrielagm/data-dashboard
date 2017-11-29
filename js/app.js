4e2031399be2343c02d60cc38717e74810773d2a
EstrellaBF <stern.von.nord@gmail.com>
2017-11-29 18:19:16 -0500

    continuando buscando una solución para que no se repita

--- js/app.js
+++ js/app.js
@@ -1,41 +1,58 @@
 window.addEventListener('load', function(event){
 
-    var chooseGeneration = document.querySelector('.choose-sede-generation');
-    var optgroupt =document.getElementById('optgroupt');
-    var generation = document.getElementById('generation');
-    var enrollment = document.getElementById('enrollment');
+  var chooseGeneration = document.querySelector('.choose-sede-generation');
+  var optgroupt =document.getElementById('optgroupt');
+  var generation = document.getElementById('generation');
+  var enrollment = document.getElementById('enrollment');
   
-    optgroupt.addEventListener('change', function(event){
-        debugger
-        console.log(event.target.value)
-        for(var i = 0; i < Object.keys(data).length; i++){
-            if(event.target.value === Object.keys(data)[i]){
-                for(var j = 0; j < Object.keys(data[Object.keys(data)[i]]).length; j++){
-                    var listGeneration = document.createElement('option');
-                    listGeneration.classList.add('generation');
-                    var textGeneration = document.createTextNode(Object.keys(data[Object.keys(data)[i]])[j]);
-                    listGeneration.appendChild(textGeneration);
-                    generation.appendChild(listGeneration);
-                }
- 
-            }
-        }
-      
-        generation.classList.remove('display-none');
-       
-        generation.addEventListener('change', function(event){
-            console.log(event.target.value);
+  optgroupt.addEventListener('change', function(event){
+    //debugger
+    //console.log(event.target.value);
+    
+    for(var i = 0; i <= Object.keys(data).length; i++){
+      if(event.target.value === Object.keys(data)[i]){
+        for(var j = 0; j < Object.keys(data[Object.keys(data)[i]]).length; j++){
+          /*creando los option */
+          var listGeneration = document.createElement('option');
+          listGeneration.classList.add('generation');
+          var textGeneration = document.createTextNode(Object.keys(data[Object.keys(data)[i]])[j]);
+          listGeneration.appendChild(textGeneration);
+          generation.appendChild(listGeneration);
+          /*hacer un bucle para que recorra el contenido de Generation y si tiene contenido igual a sede, dejarlo, si no , borrarlo*/
+          if(generation[j]){
+            console.log(generation[j].value);
+          } /*falta var classie = document.getElementsByClassName[1]
+          undefined
+          classie
+          undefined
+          classie = document.getElementsByClassName('generation')[1]
+          <option class=​"generation">​2016-2​</option>​
+          classie
+          <option class=​"generation">​2016-2​</option>​
+          classi = document.getElementsByClassName('generation')
+          (3) [select#generation.generation, option.generation, option.generation, generation: select#generation.generation]
+          classi[1]
+          <option class=​"generation">​2016-2​</option>​
+          llamar por clase y posición i o j
+          */
+        }    
+      }
+    }
+  });
 
-        });
-
-
-
-    });
-
+    
 });
+
+    
+    /*generation.addEventListener('toggle', myFunction);
+    });  
+    function myFunction() {
+      alert("The ontoggle event occured.");
+    } */
  
 
 //debugger
+/*
 console.log(Object.keys(data));//["AQP", "CDMX", "LIM", "SCL"]
 console.log(Object.values(data));//[{…}, {…}, {…}, {…}]
 console.log(Object.values(data)[0]);
@@ -45,7 +62,6 @@ console.log(Object.values(data)[0])
 console.log(Object.keys(Object.values(data)[0]).length);
 //console.log(Object.keys(Object.keys(data)));
 var sedes=Object.keys(data);
-var array = [];
 //debugger
 /*
 Para crear dinámicamente el select que está en el lado derecho.
